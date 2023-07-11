74495b8c3b45dfc4a85d0fe4b3ffb2ad91eaca0f
Shuntaro Aoki <s_aoki@i.kyoto-u.ac.jp>
2023-07-11 17:11:00 +0900

    [update] add ImageNetTraining mean features in download.sh

--- reconstruction/data/download.sh
+++ reconstruction/data/download.sh
@@ -4,3 +4,6 @@ set -Ceu
 
 [ -e decoded_features-ImageNetTest-deeprecon_originals-VGG19.zip ] || wget https://figshare.com/ndownloader/files/40106476 -O decoded_features-ImageNetTest-deeprecon_originals-VGG19.zip
 unzip -n decoded_features-ImageNetTest-deeprecon_originals-VGG19.zip
+
+[ -e features-ImageNetTraining-VGG_ILSVRC_19_layers-mean.mat.zip ] || wget https://figshare.com/ndownloader/files/41520621 -O features-ImageNetTraining-VGG_ILSVRC_19_layers-mean.mat.zip
+unzip -n features-ImageNetTraining-VGG_ILSVRC_19_layers-mean.mat.zip
