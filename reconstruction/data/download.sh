4cab7235e619c9c958c4f915e1e3780267cd4fd4
Shuntaro Aoki <s_aoki@i.kyoto-u.ac.jp>
2023-08-24 11:26:29 +0900

    [update] update data dir

--- reconstruction/data/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -Ceu
-
-[ -e decoded_features-ImageNetTest-deeprecon_originals-VGG19.zip ] || wget https://figshare.com/ndownloader/files/40106476 -O decoded_features-ImageNetTest-deeprecon_originals-VGG19.zip
-unzip -n decoded_features-ImageNetTest-deeprecon_originals-VGG19.zip
-
-[ -e features-ImageNetTraining-VGG_ILSVRC_19_layers-mean.mat.zip ] || wget https://figshare.com/ndownloader/files/41520621 -O features-ImageNetTraining-VGG_ILSVRC_19_layers-mean.mat.zip
-unzip -n features-ImageNetTraining-VGG_ILSVRC_19_layers-mean.mat.zip
